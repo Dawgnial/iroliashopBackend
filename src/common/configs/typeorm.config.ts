3a6aaaf657ff1fbaeec21b37180e448997ca0e1d
Dawgnial <dnlvb69@gmail.com>
2025-06-11 03:49:00 +0330

    fixing db

--- src/common/configs/typeorm.config.ts
+++ src/common/configs/typeorm.config.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
-import { join } from 'path';
 
 @Injectable()
 export class TypeOrmConfig implements TypeOrmOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    console.log('DB_HOST:', this.configService.get('DB_HOST'));
-    console.log('DB_PORT:', this.configService.get('DB_PORT'));
-    console.log('DB_USERNAME:', this.configService.get('DB_USERNAME'));
-    console.log('DB_PASSWORD:', this.configService.get('DB_PASSWORD'));
-    console.log('DB_NAME:', this.configService.get('DB_NAME'));
+    const dbHost = this.configService.get<string>('DB_HOST');
+    const dbPort = parseInt(this.configService.get<string>('DB_PORT') || '3306');
+    const dbUser = this.configService.get<string>('DB_USERNAME');
+    const dbPass = this.configService.get<string>('DB_PASSWORD');
+    const dbName = this.configService.get<string>('DB_NAME');
+
+    if (!dbHost || !dbPort || !dbUser || !dbPass || !dbName) {
+      throw new Error(
+        `❌ Missing database configuration:
+        DB_HOST=${dbHost}
+        DB_PORT=${dbPort}
+        DB_USERNAME=${dbUser}
+        DB_PASSWORD=${dbPass ? '***' : ''}
+        DB_NAME=${dbName}
+        `
+      );
+    }
 
     return {
       type: 'mysql',
-      host: this.configService.get<string>('DB_HOST'),
-      port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get<string>('DB_USERNAME'),
-      password: this.configService.get<string>('DB_PASSWORD'),
-      database: this.configService.get<string>('DB_NAME'),
+      host: dbHost,
+      port: dbPort,
+      username: dbUser,
+      password: dbPass,
+      database: dbName,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: false,

6a25999932c660e1613b36faaa48471dd7d7eedb
Dawgnial <dnlvb69@gmail.com>
2025-06-11 04:07:25 +0330

    fixing db

--- src/common/configs/typeorm.confg.ts
+++ src/common/configs/typeorm.confg.ts
@@ -1,20 +1,22 @@
-import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { ENV } from '../constant/env';
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
+@Injectable()
 export class TypeOrmConfig implements TypeOrmOptionsFactory {
-  createTypeOrmOptions(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'mysql',
-      host: ENV.host,
-      username: ENV.username,
-      password: ENV.password,
-      database: ENV.database,
-      port: ENV.port,
+      host: this.configService.get<string>('DB_HOST'),
+      port: Number(this.configService.get<number>('DB_PORT')),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
-      entities: [
-        'dist/**/**/**/*.entity{.ts,.js}',
-        'dist/**/**/*.entity{.ts,.js}',
-      ],
+      logging: false,
     };
   }
 }

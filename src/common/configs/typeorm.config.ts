4020742b16936809323baf73ef67e0372fbb501b
Dawgnial <dnlvb69@gmail.com>
2025-06-11 03:57:51 +0330

    dbi

--- src/common/configs/typeorm.config.ts
+++ src/common/configs/typeorm.config.ts
@@ -7,8 +7,17 @@ export class TypeOrmConfig implements TypeOrmOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    console.log('🔍 ENV CHECK:');
+    console.log('DB_HOST:', this.configService.get('DB_HOST'));
+    console.log('DB_PORT:', this.configService.get('DB_PORT'));
+    console.log('DB_USERNAME:', this.configService.get('DB_USERNAME'));
+    console.log('DB_PASSWORD:', this.configService.get('DB_PASSWORD'));
+    console.log('DB_NAME:', this.configService.get('DB_NAME'));
+
     const dbHost = this.configService.get<string>('DB_HOST');
-    const dbPort = parseInt(this.configService.get<string>('DB_PORT') || '3306');
+    const dbPort = parseInt(
+      this.configService.get<string>('DB_PORT') || '3306',
+    );
     const dbUser = this.configService.get<string>('DB_USERNAME');
     const dbPass = this.configService.get<string>('DB_PASSWORD');
     const dbName = this.configService.get<string>('DB_NAME');
@@ -21,7 +30,7 @@ export class TypeOrmConfig implements TypeOrmOptionsFactory {
         DB_USERNAME=${dbUser}
         DB_PASSWORD=${dbPass ? '***' : ''}
         DB_NAME=${dbName}
-        `
+        `,
       );
     }
 

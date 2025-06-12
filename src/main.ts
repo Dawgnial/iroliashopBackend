79a9e93ee44e4ac05991f68a65d792912e030d18
Dawgnial <dnlvb69@gmail.com>
2025-06-12 04:05:42 +0330

    Listen Error Fix

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,11 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.enableCors({
-  origin: 'https://www.iroliashop.ir',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-});
-
-await app.listen(3000);
-
+  app.enableCors({
+    origin: 'https://www.iroliashop.ir',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),

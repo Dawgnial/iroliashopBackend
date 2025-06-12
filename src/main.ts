bae590b2bbe924c764ba525306a855ab03cf8696
Dawgnial <dnlvb69@gmail.com>
2025-06-12 03:58:11 +0330

    fixing database

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,15 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+app.enableCors({
+  origin: 'https://www.iroliashop.ir',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+});
+
+await app.listen(3000);
+
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
